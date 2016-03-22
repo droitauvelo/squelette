8d34ba37638efe435cf889a5fc66b6668d7b1a90
Guillaume Wauquier <guillaume.wauquier@declic3000.com>
2016-03-22 04:53:09 +0100

    moultes modifications

--- js/sjs.js
+++ js/sjs.js
@@ -13,8 +13,9 @@ $(document).ready(function(){
 	 $('.bouton_'+id_menu).hover(function(event){
 		 if($(window).width()>768){
 		 	$(this).addClass("hover");
-			var position = $(this).position();
-			$('#'+id_menu).css('left',position.left/2);
+			//var position = $(this).position();
+			//console.log($(this).position());
+			//$('#'+id_menu).css('left',position.left/2);
 			$('#'+id_menu).stop(true,true).delay(20).slideDown();
 		}
 		},function(event){
@@ -25,14 +26,18 @@ $(document).ready(function(){
 		});
 
  });
- $('.bloc_contact .texte_suppl').hide();
+
+	$('.bloc_contact .texte_suppl').hide();
+
+
 $('.bloc_contact').hover(function(){
-	$('span',this).stop().animate({'line-height':1.2,'font-size':'0.9em','margin-left':'0.5em','margin-right':'0.5em'});
+	$('span',this).stop().animate({'margin-top':'2px','line-height':1.2,'font-size':'0.9em','margin-left':'0.5em','margin-right':'0.5em'});
 	$('.bloc_contact .texte_suppl').stop().fadeIn();
 
 	},
 	function(){
-	$('span',this).stop().animate({'line-height':2.5,'font-size':'1em','margin-left':'0em','margin-right':'0em'});
+	$('span',this).stop().animate({
+		'margin-top':'1em','line-height':2.5,'font-size':'1em','margin-left':'0em','margin-right':'0em'});
 	$('.bloc_contact .texte_suppl').stop().fadeOut();
 
 	}
